7bc5da63e71b3badaba8d4b8c57bb366927ef505
YasinCoban123 <9018681@student.zadkine.nl>
2023-05-19 00:05:35 +0100

    s

--- pages/page.js
+++ pages/page.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import styles from './globals.css';
 import Nav from './Nav';
 import React from "react";
 import Link from "next/link";
